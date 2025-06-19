6ed970ce69a9e2a50e0e1093d7308807d778a4cf
Henry McKay <56372705+hhmckay@users.noreply.github.com>
2025-06-19 11:02:26 -0700

    Update Cost_Summary.R

--- Cost_Summary.R
+++ Cost_Summary.R
@@ -2,12 +2,15 @@ library(dplyr)
 library(sf)
 library(mapview)
 
-projects <- read.csv("/Users/S152973/Downloads/EQI_Lookback_Cost/active_transportation_projects.csv")
+projects <- read.csv("/Users/S152973/Downloads/EQI_Lookback_Cost/reactive_safety_projects.csv")
 
-costs <- read.csv("/Users/S152973/Downloads/EQI_Lookback_Cost/active_transportation_costs.csv") %>%
-  filter(Activity.Category == "Bicycle and Pedestrian Infrastructure") %>%
+costs <- read.csv("/Users/S152973/Downloads/EQI_Lookback_Cost/reactive_safety_costs.csv") %>%
+  filter(Activity.Category == "Reactive Safety") %>%
   group_by(ID) %>%
-  summarise(Total_Cost = (sum(Total_Cost, na.rm = T) * 1000))
+  summarise(Total_Cost = (sum(Total_Cost, na.rm = T) * 1000),
+            RW_Cost = (sum(RW_Cost, na.rm = T) * 1000),
+            Const_Cost = (sum(Const_Cost, na.rm = T) * 1000),
+            Support_Cost = (sum(Support_Cost, na.rm = T) * 1000))
 
 project_df <- merge(projects,
                     costs,
@@ -15,4 +18,4 @@ project_df <- merge(projects,
                     by.y = "ID",
                     all.x = T)
 
-write.csv(project_df, "/Users/S152973/Downloads/EQI_Lookback_Cost/active_transportation_analysis.csv")
+write.csv(project_df, "/Users/S152973/Downloads/EQI_Lookback_Cost/reactive_safety_analysis_v2.csv")
